bd987871accb4cacba8c367a433d684c4f108ab8
Karaargirov Bozhidar (INST-IOT/ESS4) <Bozhidar.Karaargirov@bosch-si.com>
2019-11-01 08:21:57 +0200

    sync

---
+++ src/main/scala/hypedex/services/QueryAnalysisService.scala
@@ -0,0 +1,7 @@
+package hypedex.services
+
+import hypedex.logicalEngine.model.AccumulatedFilter
+
+class QueryAnalysisService {
+  def createFilters(query: String): Map[String, AccumulatedFilter] = ???
+}
